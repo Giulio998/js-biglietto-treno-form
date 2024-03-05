50461bc8049dc347719b9d6003f15501bd788aeb
Giulio998 <giulio_favia@hotmail.it>
2024-03-05 15:13:29 +0100

    Milestone 2

--- js/app.js
+++ js/app.js
@@ -1,25 +1,38 @@
 function myFunction() {
     let km = document.getElementById('km').value;
-    let age = document.getElementById('age').value;
+    let discount = document.getElementById('discount').value;
     
     const basePrice = (km * 0.21);
     let finalPrice;
     let minorPrice = basePrice - (basePrice * 0.2);
+    const minorDiscount = "20%";
+    const seniorDiscount = "40%";
+
     let seniorPrice = basePrice - (basePrice * 0.4);
 
-    console.log("km", km);
-    console.log("age", age);
 
-    if (age < 18) {
+
+
+    console.log("km ", km);
+    console.log("basePrice ", basePrice);
+    console.log("discount ", discount);
+
+
+      if (discount == "minorenne") {
         finalPrice = minorPrice.toFixed(2);
-        console.log("minorPrice", finalPrice);
-    } else if (age > 65) {
+        console.log("minorDiscount ", minorDiscount);
+        console.log("minorPrice ", finalPrice);
+    } else if (discount == "over 65") {
         finalPrice = seniorPrice.toFixed(2);
-        console.log("seniorPrice", finalPrice);
+        console.log("seniorDiscount ", seniorDiscount);
+        console.log("seniorPrice ", finalPrice);
     } else {
         finalPrice = basePrice.toFixed(2);
-        console.log("basePrice", finalPrice);
-    }
+        console.log("basePrice ", finalPrice);
+    } 
+
+
+
 }
 
     
